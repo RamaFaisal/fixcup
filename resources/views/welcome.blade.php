2dc3ab9532ab27ae9d2c0d05f094ad670fe79832
Rama Faisal <helloramafaisal@gmail.com>
2025-04-02 09:15:41 +0700

    Init Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fixcup 6.0</title>
+    <title>FIXCUP 6.0</title>
     @vite(['resources/css/app.css'])
 
     {{-- Poppins Font --}}
